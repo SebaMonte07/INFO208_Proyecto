8aacc6713dcf5d2d28d5f240e537e3cfea711b34
Lewis <63936002+Luis-iom@users.noreply.github.com>
2023-12-10 14:21:35 -0300

    Se mejoró levemente lo visual del LoginScreen.js
    se cambio la tonalidad de la pagina y se agregó un aviso al ingresar credenciales incorrectas, aparte de otros arreglos visuales leves y ordenamiento de codigo

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -16,49 +16,58 @@ const LoginScreen = ({ navigation }) => {
     { username: 'Secretaria', password: '123' },
     // ... otros usuarios ...
   ]);
+  const [showError, setShowError] = useState(false);
 
   const handleLogin = () => {
     const user = users.find((user) => user.username === username && user.password === password);
     if (user) {
-      console.log("Usuario ha iniciado sesión");
       Alert.alert("Credenciales correctas");
+      console.log("Usuario ha iniciado sesión");
       navigation.navigate('Opciones', { username: username });
     } else {
       Alert.alert("Error al iniciar sesión!");
+      console.log('Credenciales incorrectas');
+      setShowError(true); // Muestra el aviso de credenciales incorrectas
     }
   };
 
   return (
     <View style={styles.container}>
       <Image source={require('../assets/logoinformatica.jpg')} style={{ width: 200, height: 117, marginTop: -100 }} />
+
       <View style={styles.container2}>
         <Text style={styles.titulo}>Inicio de sesión</Text>
-        <View style={styles.containerRow}>
-          <Icon name="user" size={25} color='white' />
+        <View style={styles.containerInputs}>
+          <Icon name="user" size={25} color='white'/>
           <TextInput
             style={styles.input}
-            placeholder='Nombre de usuario...'
+            placeholder='Nombre de usuario'
             onChangeText={text => setUsername(text)}
             value={username}
           />
         </View>
-        <View style={styles.containerRow}>
+        <View style={styles.containerInputs}>
           <Icon name="lock" size={25} color='white' />
           <TextInput
             style={styles.input}
-            placeholder='Contraseña...'
+            placeholder='Contraseña'
             onChangeText={text => setPassword(text)}
             value={password}
             secureTextEntry={true}
           />
         </View>
 
+        {showError && (
+        <Text style={styles.textoErrorCredenciales}>Credenciales Incorrectas</Text>
+        )}
+
         <TouchableOpacity onPress={handleLogin} style={{ width: 100 }}>
-          <Text style={{ fontWeight: 'normal', fontSize: 15, backgroundColor: '#50a5e6', borderRadius: 10, textAlign: 'center', color: 'white' }}>
+          <Text style={{ fontWeight: 'normal', fontSize: 15, backgroundColor: '#00994D', borderRadius: 10, textAlign: 'center', color: 'white' }}>
             INGRESAR
           </Text>
         </TouchableOpacity>
       </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -67,13 +76,16 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fcd381',
+    backgroundColor: '#E0FFFF',
+    //backgroundColor: '#fcd381',
     justifyContent: 'center',
     alignItems: 'center',
   },
 
+// Recuadro interno (debajo del logo)
   container2: {
-    backgroundColor: '#fab778',
+    backgroundColor: 'skyblue',
+    //backgroundColor: '#fab778',
     height: 240,
     width: 300,
     alignItems: 'center',
@@ -81,23 +93,37 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
 
-  containerRow: {
+// Recuadro que contiene un 'simbolo' y un rectangulo 'input'
+  containerInputs: {
+    //backgroundColor: 'black',
     alignItems: 'center',
     flexDirection: 'row',
   },
 
+// Rectangulo para ingresar algo 'input'
   input: {
     backgroundColor: 'white',
     borderWidth: 1,
     textAlign: 'center',
     width: 200,
+    height: 25,
     margin: 15,
     borderRadius: 20
   },
 
   titulo: {
+    fontFamily: 'Georgia',
     fontSize: 20,
     marginBottom: 0,
+  },
+
+  textoErrorCredenciales: {
+    //backgroundColor: 'black',
+    fontSize: 16,
+    color: 'red',
+    textAlign: 'center',
+    paddingBottom: 15,
+    fontWeight: 'bold',
   },
 });
 
